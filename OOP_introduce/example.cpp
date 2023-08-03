992d7b9b14f9388e4a642eb5f7d5e6642a9c4b67
DUC DO <duc1023as@gmail.com>
2023-08-03 16:51:53 +0700

    change introduce

--- OOP_introduce/example.cpp
+++ OOP_introduce/example.cpp
@@ -1,31 +1,25 @@
 #include<iostream>
 #include<string>
 
-class IntPair {
+class RectAngle {
+private:
+    int width;
+    int height;
 public:
-    int m_first {};
-    int m_second {};
-    void set(int n1,int n2){
-        m_first = n1;
-        m_second = n2;
-    }
-    void print(){
-        std::cout<<"Pair("<<m_first<<","<<m_second<<")"<<std::endl;
-    }
+    void set_values(int,int);
+    int area() {return width*height;}
 };
 
+void RectAngle::set_values(int _width, int _height){
+    width = _width;
+    height = _height;
+}
 
 int main(){
-	IntPair p1;
-	p1.set(1, 1); // set p1 values to (1, 1)
-
-	IntPair p2 { 2, 2 }; // initialize p2 values to (2, 2)
-
-	p1.print();
-	p2.print();
-
-    int x {};
-    std::cout<<x<<std::endl;
-	return 0;
-
+    RectAngle r1;
+    RectAngle r2;
+    r1.set_values(10,5);
+    r2.set_values(16,3);
+    std::cout<<"R1:"<<r1.area()<<" R2:"<<r2.area()<<std::endl;
+    return 0;
 }
