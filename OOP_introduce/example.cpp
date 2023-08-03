2c9d6da9d5462654a73a7cd5d2826ea04b551c3f
DUC DO <duc1023as@gmail.com>
2023-08-03 17:03:32 +0700

    add constructor

--- OOP_introduce/example.cpp
+++ OOP_introduce/example.cpp
@@ -6,20 +6,24 @@ private:
     int width;
     int height;
 public:
-    void set_values(int,int);
+    // void set_values(int,int);
+    RectAngle(int _width, int _height){
+        width = _width;
+        height = _height;
+    }
     int area() {return width*height;}
 };
 
-void RectAngle::set_values(int _width, int _height){
-    width = _width;
-    height = _height;
-}
+// void RectAngle::set_values(int _width, int _height){
+//     width = _width;
+//     height = _height;
+// }
 
 int main(){
-    RectAngle r1;
-    RectAngle r2;
-    r1.set_values(10,5);
-    r2.set_values(16,3);
+    RectAngle r1(10,5);
+    RectAngle r2(16,3);
+    // r1.set_values(10,5);
+    // r2.set_values(16,3);
     std::cout<<"R1:"<<r1.area()<<" R2:"<<r2.area()<<std::endl;
     return 0;
 }
