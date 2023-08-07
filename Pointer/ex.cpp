ff8bd6e06a56c362daca9c6c867cb4b2b8792544
DUC DO <duc1023as@gmail.com>
2023-08-07 16:37:12 +0700

    NULL Pointerr

--- Pointer/ex.cpp
+++ Pointer/ex.cpp
@@ -5,6 +5,8 @@ int main(){
     int x = 100;
     const int *ptr = &x;
     std::cout<<*ptr<<std::endl;
+    int *p1 = 0;
+    int *p2 = nullptr;
     // std::cout<<(*ptr)++<<std::endl; //can't modify
     /*
     int x;
